dca0a95daf8de3eab71d6c13b4947bba2a478d94
Jean-Yves TRAN <jendives2000@gmail.com>
2024-03-13 10:27:58 +0200

    Finished the main file

---
+++ main.py
@@ -0,0 +1,13 @@
+# Exercises from the book Python Crash Course by Eric Matthes.
+# These exercises are about Storing Data (write) and using it (read) via the json package but also refactoring code into functions.
+
+# -----------------------------------------------------------------------------
+print("Exercise 10-11:\n-------------------------------\n")
+from exercise_10_11 import read_fav_num
+from exercise_10_11 import write_fav_num
+
+print("Exercise 10-12:\n-------------------------------\n")
+from exercise_10_12 import exercise_10_12
+
+print("Exercises 10-13_and_10_14:\n-------------------------------\n")
+from exercises_10_13_and_10_14 import exercise_10_13_and_10_14
